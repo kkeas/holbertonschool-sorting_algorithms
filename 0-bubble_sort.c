0d479a552d55326993497c60ceaf7146ac858d79
kkeas <keaskatrina@gmail.com>
2022-12-13 14:42:59 -0800

    all hail betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,23 +1,30 @@
 #include "sort.h"
+
+/**
+*bubble_sort - sorts array in ascending order
+*@array: array to be sorted
+*@size: size of array
+*Return: none
+*/
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j, swap;
+	size_t i, j, swap;
 
-    /* loop to compare each array element*/
-    for (i = 0; i < size - 1; ++i)
-    {
-    /*loop to compare array elements*/
-        for (j = 0; j < size - i - 1; ++j)
-        {
-    /*compare two adjacent elements*/
-            if (array[j] > array[j + 1])
-            {
-                swap = array[j];
-                array[j] = array[j+1];
-                array[j+1] = swap;
-                print_array(array, size);
-            }
-        }
-    }
+	/* loop to compare each array element*/
+	for (i = 0; i < size - 1; ++i)
+	{
+	/*loop to compare array elements*/
+		for (j = 0; j < size - i - 1; ++j)
+		{
+	/*compare two adjacent elements*/
+			if (array[j] > array[j + 1])
+			{
+				swap = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = swap;
+				print_array(array, size);
+			}
+		}
+	}
 }
 
